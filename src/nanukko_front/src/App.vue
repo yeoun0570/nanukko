bf46e38f3a7c6d9440a10841caa24f8bfdf76cfc
여운 <yeoun0570@gmail.com>
2024-11-04 21:29:52 +0900

    :sparkles: 나누꼬 패키지 구조 초안 (Nuxt 적용X)

---
+++ src/nanukko_front/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import ConnectionTest from './components/ConnectionTest.vue';
+</script>
+
+<template>
+<ConnectionTest></ConnectionTest>
+</template>
+
+<style scoped>
+</style>
---
+++ src/nanukko_front/src/components/ConnectionTest.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+export default {
+  setup() {
+    const data = ref("로딩 중...");
+
+    onMounted(async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/api/test"); // 8080 포트로 요청
+        data.value = response.data;
+      } catch (error) {
+        console.error("Error fetching data:", error); // 에러 로그 확인
+      }
+    });
+
+    return { data };
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <h1>백엔드에서 받은 데이터:</h1>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
