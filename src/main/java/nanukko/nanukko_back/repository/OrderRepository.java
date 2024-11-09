7c630eef95ae743fdc1bc39ef86827fe3f1ef986
여운 <yeoun0570@gmail.com>
2024-11-09 22:44:12 +0900

    :recycle: 결제 상태와 에스크로 데드라인 날짜를 받아서 데드라인 지나면 자동으로 구매확정 시킬 수 있는 메서드 추가

--- src/main/java/nanukko/nanukko_back/repository/OrderRepository.java
+++ src/main/java/nanukko/nanukko_back/repository/OrderRepository.java
@@ -1,9 +1,17 @@
 package nanukko.nanukko_back.repository;
 
 import nanukko.nanukko_back.domain.order.Orders;
+import nanukko.nanukko_back.domain.order.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Orders, String> {
+    List<Orders> findByStatusAndEscrowDeadlineBefore(
+            PaymentStatus status,
+            LocalDateTime deadline
+    );
 }
