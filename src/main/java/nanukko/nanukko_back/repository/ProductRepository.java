4ff074176246bd725a18b8773ba7539a82a4548b
여운 <yeoun0570@gmail.com>
2024-11-10 21:22:33 +0900

    :recycle: 마이페이지 판매상품 조회를 위해 사용될 메서드 추가(+페이징처리)

--- src/main/java/nanukko/nanukko_back/repository/ProductRepository.java
+++ src/main/java/nanukko/nanukko_back/repository/ProductRepository.java
@@ -1,9 +1,16 @@
 package nanukko.nanukko_back.repository;
 
 import nanukko.nanukko_back.domain.product.Product;
+import nanukko.nanukko_back.domain.product.ProductStatus;
+import nanukko.nanukko_back.domain.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findBySellerAndStatusOrderByCreatedAtDesc(User seller, ProductStatus status, Pageable pageable);
 }
