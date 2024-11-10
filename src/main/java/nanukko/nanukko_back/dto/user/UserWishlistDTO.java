9350120973a20703f004e53caa083c7a4096d869
여운 <yeoun0570@gmail.com>
2024-11-11 00:19:10 +0900

    :sparkles: 마이페이지 위시리스트 조회를 위해 사용할 DTO 생성

---
+++ src/main/java/nanukko/nanukko_back/dto/user/UserWishlistDTO.java
@@ -0,0 +1,18 @@
+package nanukko.nanukko_back.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nanukko.nanukko_back.domain.product.ProductStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserWishlistDTO {
+    private String thumbnailImage;
+    private String productName;
+    private int price;
+    private ProductStatus status;
+}
