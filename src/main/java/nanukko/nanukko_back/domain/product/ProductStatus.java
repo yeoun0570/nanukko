314f68e5a5cbcae3229e4da1c503311f94e99d4a
여운 <yeoun0570@gmail.com>
2024-11-10 15:07:21 +0900

    :recycle: NEW 상태를 SELLING 으로 변경

--- src/main/java/nanukko/nanukko_back/domain/product/ProductStatus.java
+++ src/main/java/nanukko/nanukko_back/domain/product/ProductStatus.java
@@ -1,7 +1,7 @@
 package nanukko.nanukko_back.domain.product;
 
 public enum ProductStatus {
-    NEW, //새로운 상품
+    SELLING, //판매중인 상품
     RESERVED, //예약된 상품
     SOLD_OUT, //판매완료된 상품
     REMOVED //삭제된 상품
