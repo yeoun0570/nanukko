66fdb01da6bc8863bc8b65fa79bb66524451f61b
여운 <yeoun0570@gmail.com>
2024-11-10 21:20:50 +0900

    :sparkles: 페이징 처리해서 프론트로 보낼 DTO

---
+++ src/main/java/nanukko/nanukko_back/dto/page/PageResponseDTO.java
@@ -0,0 +1,37 @@
+package nanukko.nanukko_back.dto.page;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//페이징 처리를 프론트를 전달하기 위한 DTO
+public class PageResponseDTO<T> {
+    private List<T> content;        //실제 데이터
+    private int totalPages;         //전체 페이지수
+    private Long totalElements;     //전체 데이터수
+    private int currentPage;        //현재 페이지
+    private int size;               //페이지 크기
+    private boolean first;          //첫 페이지 여부
+    private boolean last;           //마지막 페이지 여부
+    private boolean hasNext;        //다음 페이지 존재 여부
+    private boolean hasPrevious;    //이전 페이지 존재 여부
+
+    //Page 객체로부터 PageResponseDTO를 생성하는 생성자
+    public PageResponseDTO(Page<T> page) {
+        this.content = page.getContent();
+        this.totalPages = page.getTotalPages();
+        this.totalElements = page.getTotalElements();
+        this.currentPage = page.getNumber();
+        this.size = page.getSize();
+        this.first = page.isFirst();
+        this.last = page.isLast();
+        this.hasNext = page.hasNext();
+        this.hasPrevious = page.hasPrevious();
+    }
+}
