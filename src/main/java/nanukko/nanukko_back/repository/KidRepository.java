155e8c5820e99f26b335b97196d36d89925be7a0
여운 <yeoun0570@gmail.com>
2024-11-11 16:07:39 +0900

    :recycle: JpaRepository 키값 String에서 KidId로 변경, KidId 추가를 위한 메서드 추가

--- src/main/java/nanukko/nanukko_back/repository/KidRepository.java
+++ src/main/java/nanukko/nanukko_back/repository/KidRepository.java
@@ -1,13 +1,18 @@
 package nanukko.nanukko_back.repository;
 
 import nanukko.nanukko_back.domain.user.Kid;
+import nanukko.nanukko_back.domain.user.KidId;
 import nanukko.nanukko_back.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface KidRepository extends JpaRepository<Kid, Long> {
+public interface KidRepository extends JpaRepository<Kid, KidId> {
+    //자녀 조회를 위한 메서드
     List<Kid> findByUserOrderByKidId(User user);
+    //자녀 KidId 추가를 위한 메서드
+    Optional<Kid> findByUserAndKidId(User user, String kidId);
 }
