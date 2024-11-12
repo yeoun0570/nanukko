0a2fb3c867b47993ade3ce9d89a2e4b4ee32ac74
여운 <yeoun0570@gmail.com>
2024-11-12 12:03:30 +0900

    :recycle: 상품 상세설명 테이블에서 컬럼으로 변경 + 컬럼 추가로 인한 수정

--- src/main/java/nanukko/nanukko_back/dto/user/UserSetProductDTO.java
+++ src/main/java/nanukko/nanukko_back/dto/user/UserSetProductDTO.java
@@ -1,7 +1,5 @@
 package nanukko.nanukko_back.dto.user;
 
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
