c183872748a08316681b89e5d096ec3aa6652558
여운 <yeoun0570@gmail.com>
2024-11-12 09:26:56 +0900

    :recycle: DTO에 대한 설명 주석 추가

--- src/main/java/nanukko/nanukko_back/dto/user/UserProductDTO.java
+++ src/main/java/nanukko/nanukko_back/dto/user/UserProductDTO.java
@@ -10,7 +10,7 @@ import nanukko.nanukko_back.domain.product.ProductStatus;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-//사용자의 판매 상품 조회하기 위한 DTO
+//마이페이지에서 사용자의 판매 상품 조회하기 위한 DTO
 public class UserProductDTO {
     private String thumbnailImage;
     private String productName;
