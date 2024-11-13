88c7317b91579a12b268832a76fed8cc6ef42817
여운 <yeoun0570@gmail.com>
2024-11-13 12:52:19 +0900

    :sparkles: 자신의 거래 후기 조회하기 위한 DTO 작업

---
+++ src/main/java/nanukko/nanukko_back/dto/review/ReviewInMyStoreDTO.java
@@ -0,0 +1,27 @@
+package nanukko.nanukko_back.dto.review;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+//내 상점에서 후기 조회하기 위한 DTO
+public class ReviewInMyStoreDTO {
+    private Long reviewId;
+    private Long productId;
+    private String productName;
+    private String authorId;
+    private String profile;
+    private String authorNickName;
+    private String review;
+    private int rate;
+    @Min(0)
+    @Max(100)
+    private double reviewRate; //상점 평점
+}
