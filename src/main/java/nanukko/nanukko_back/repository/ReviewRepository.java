9d0069fa288638a4f40a5373bbc4e2cd0b66a77f
여운 <yeoun0570@gmail.com>
2024-11-13 12:53:33 +0900

    :sparkles: 거래 후기 JPA Repository 작업 - 상점 평점 평균 구하는 메서드 추가 + 자신의 상점의 후기 조회하기 위한 메서드 추가

---
+++ src/main/java/nanukko/nanukko_back/repository/ReviewRepository.java
@@ -0,0 +1,20 @@
+package nanukko.nanukko_back.repository;
+
+import nanukko.nanukko_back.domain.review.Review;
+import nanukko.nanukko_back.domain.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    // 판매자의 상점 후기 평균 구하기
+    @Query("SELECT COALESCE(AVG(r.rate), 50.0) FROM Review r WHERE r.product.seller = :seller")
+    double averageRateByProductSeller(@Param("seller") User seller);
+
+    //내 상점에서 후기 조회
+    Page<Review> findByProductSellerOrderByCreatedAtDesc(User user, Pageable pageable);
+}
