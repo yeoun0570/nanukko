51cf450036e049f1cd6873e65ad8e62bc821e87c
여운 <yeoun0570@gmail.com>
2024-11-14 23:51:30 +0900

    :sparkles: 결제 프론트 작업

---
+++ src/nanukko_front/components/payments/PaymentButton.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  loading: {
+    type: Boolean,
+    default: false
+  }
+});
+
+defineEmits(['startPayment']);
+</script>
+
+<template>
+  <button @click="$emit('startPayment')" :disabled="loading" class="payment-button">
+    결제하기
+  </button>
+</template>
+
+<style scoped>
+@import url('../../assets/payments/PaymentButton.css');
+</style>
---
+++ src/nanukko_front/components/payments/PaymentCancelButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+    productData: {
+        type: Object,
+    }
+});
+
+defineEmits(['cancelOrder']);
+</script>
+
+<template>
+  <button
+    v-show="productData && productData.status === 'ESCROW_HOLDING'"
+    @click="$emit('cancelOrder')"
+    class="cancel-btn"
+  >
+    결제 취소
+  </button>
+</template>
