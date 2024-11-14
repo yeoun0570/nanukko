d0ebba1d61bd91cdb9ca172e1ec9ecdd33ec9427
여운 <yeoun0570@gmail.com>
2024-11-14 23:31:47 +0900

    :recycle: 토스 페이먼츠 스크립트 추가

--- src/nanukko_front/nuxt.config.ts
+++ src/nanukko_front/nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+
+  // 컴포넌트 자동 import 설정
+  components: {
+    dirs: ["~/components", "~/components/payments"],
+  },
+
+  // 토스 페이먼츠 스크립트
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://js.tosspayments.com/v1",
+          defer: true,
+        },
+      ],
+    },
+  },
+});
