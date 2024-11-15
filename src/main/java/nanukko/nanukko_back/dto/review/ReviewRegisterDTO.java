1096da58256ceb626d6881b5165c5d73957d1944
여운 <yeoun0570@gmail.com>
2024-11-16 05:35:44 +0900

    :recycle: 리뷰 작성에서 프론트에서 받는 값 productId -> orderId로 변경 및 필요없는 필드 제거

--- src/main/java/nanukko/nanukko_back/dto/review/ReviewRegisterDTO.java
+++ src/main/java/nanukko/nanukko_back/dto/review/ReviewRegisterDTO.java
@@ -14,15 +14,10 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class ReviewRegisterDTO {
-    private Long reviewId;
-    private Long productId;
+    private String orderId;
     private String authorId; // 후기 작성자
     private String review;
     @Min(0)
     @Max(100)
     private int rate; //후기 평점
-    @Min(0)
-    @Max(100)
-    private double reviewRate; //상점 평점
-    private LocalDateTime createdAt;
 }
