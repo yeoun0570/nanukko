9c4a6c0ee5d3f28548abeb582eebe637e76be2a0
여운 <yeoun0570@gmail.com>
2024-11-15 21:14:05 +0900

    :sparkles: 프론트 페이징 컴포넌트 생성

---
+++ src/nanukko_front/components/Pageination.vue
@@ -0,0 +1,46 @@
+<script setup>
+defineProps({
+  currentPage: {
+    type: Number,
+    required: true,
+  },
+  totalPages: {
+    type: Number,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["page-change"]);
+
+const changePage = (page) => {
+  emit("page-change", page);
+};
+</script>
+
+<template>
+  <div class="pagination">
+    <button :disabled="currentPage === 0" @click="changePage(currentPage - 1)">
+      이전
+    </button>
+
+    <button
+      v-for="page in totalPages"
+      :key="page"
+      :class="{ active: currentPage === page - 1 }"
+      @click="changePage(page - 1)"
+    >
+      {{ page }}
+    </button>
+
+    <button
+      :disabled="currentPage === totalPages - 1"
+      @click="changePage(currentPage + 1)"
+    >
+      다음
+    </button>
+  </div>
+</template>
+
+<style scoped>
+@import url('../assets/Pagination.css');
+</style>
