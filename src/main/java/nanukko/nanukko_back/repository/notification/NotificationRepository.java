7bf87dd3ef6dbc134dce117364b27f714c3a2841
여운 <yeoun0570@gmail.com>
2024-11-18 00:10:45 +0900

    :recycle: 알림 받는 사람 아이디 찾기 위한 메서드 추가

--- src/main/java/nanukko/nanukko_back/repository/notification/NotificationRepository.java
+++ src/main/java/nanukko/nanukko_back/repository/notification/NotificationRepository.java
@@ -4,6 +4,10 @@ import nanukko.nanukko_back.domain.notification.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    // 알림 받는 사람 아이디 찾기
+    List<Notification> findByReceiverUserIdOrderByCreatedAtDesc(String userId);
 }
