f1bd7269e475098d88dae6f3f2a4da4fd77eb5ea
여운 <yeoun0570@gmail.com>
2024-11-21 00:31:23 +0900

    :recycle: 결제 완료 시 판매자에게 이메일 전송 가게끔 수정

--- src/main/java/nanukko/nanukko_back/service/OrderService.java
+++ src/main/java/nanukko/nanukko_back/service/OrderService.java
@@ -45,6 +45,7 @@ public class OrderService {
     private final RestTemplateConfig restTemplate;
     private final NotificationService notificationService;
     private final TossPaymentsConfig tossPaymentsConfig;
+    private final MailService mailService;
 
     //결제창 페이지에 출력할 데이터 정의
     @Transactional(readOnly = true)
@@ -114,6 +115,19 @@ public class OrderService {
                     confirmDTO.getBuyerId()
             );
 
+
+            // 결제완료 했을 시 판매자에게 메일 전송
+            // 비동기로 알림과 메일 전송
+            CompletableFuture.runAsync(() -> {
+                try {
+                    mailService.sendMailConfirmPaymentToSeller(
+                            result.getSellerId(),
+                            result.getProductId()
+                    );
+                } catch (Exception e) {
+                    log.error("알림/메일 전송 실패", e);
+                }
+            });
 
             return result;
         } catch (Exception e) {
