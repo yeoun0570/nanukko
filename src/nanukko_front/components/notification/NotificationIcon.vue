fc0bcb3a5548bb69a1c3eee38dacca6e995607c9
여운 <yeoun0570@gmail.com>
2024-11-20 19:37:18 +0900

    :recycle: 알림 레이아웃에 맞게 수정

--- src/nanukko_front/components/notification/NotificationIcon.vue
+++ src/nanukko_front/components/notification/NotificationIcon.vue
@@ -11,10 +11,15 @@ defineEmits(["toggle"]);
 
 <template>
   <div class="notification-icon" @click="$emit('toggle')">
-    <i class="fas fa-bell" :class="{ 'has-notifications': unreadCount > 0 }">
+    <span class="icon-text">알림</span>
+    <!-- 읽지 않은 알림이 있을때만 badge 표시 -->
+    <span v-if="unreadCount > 0" class="notification-badge">
+      {{ unreadCount }}
+    </span>
+    <!-- <i class="fas fa-bell" :class="{ 'has-notifications': unreadCount > 0 }">
       <span v-if="unreadCount > 0" class="notification-badge">
-        {{ unreadCount }}
+        {{ unreadCount }}F
       </span>
-    </i>
+    </i> -->
   </div>
 </template>
