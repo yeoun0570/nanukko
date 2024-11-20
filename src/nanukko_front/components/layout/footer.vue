8d647637c54bac47cbf14333bb6fdcab71758263
hyo9634 <dlgytmd5468@naver.com>
2024-11-20 12:12:54 +0900

    :recycle: 프론트 레이아웃 작업 수정

--- src/nanukko_front/components/layout/footer.vue
+++ src/nanukko_front/components/layout/footer.vue
@@ -1,11 +1,21 @@
 <template>
     <!-- Footer 영역: 사이트 하단에 표시되는 정보나 저작권 안내 -->
     <footer class="footer">
-
     <p>대표이사 : 윤여운, 신희원   |   개인정보보호책임자 : 김은강</p>
     <p>사업자등록번호 : 123-456-7890</p>
     <p>주소 : 서울특별시 강남구 어쩌구 저쩌구</p>
     <p>&copy; nanugo.inc All rights reserved.</p> 
 
     </footer>
-  </template>
+</template>
+<style scoped>
+
+    .footer {
+        font-size: 1rem;
+        display: inline;
+        text-align: center;
+        border-top: 1px solid #e0e0e0; /* 하단 경계선 */
+    }
+    
+
+</style>
--- src/nanukko_front/layouts/default.vue
+++ src/nanukko_front/layouts/default.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Header from '~/components/layout/header.vue'
 import Footer from '~/components/layout/footer.vue'
+import navigator from '~/components/layout/navigator.vue';
 </script>
 
 <template>
   <div class="layout">
     <!-- Header 컴포넌트 -->
     <Header />
+    <navigator />
 
     <!-- 메인 콘텐츠 영역 -->
     <main>
