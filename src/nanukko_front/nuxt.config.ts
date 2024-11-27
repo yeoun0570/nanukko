a0ebbc197530ec9bffd2a5459a6485b82f4c4d33
여운 <yeoun0570@gmail.com>
2024-11-27 12:26:25 +0900

    :recycle: nuxt 설정 파일 통합

--- src/nanukko_front/nuxt.config.ts
+++ src/nanukko_front/nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
     apiSecret: '',
     public: {
       kakaoMapApiKey: process.env.NUXT_PUBLIC_KAKAO_MAP_API_KEY,
-      baseURL: process.env.API_BASE_URL || 'http://localhost:8080/api'
+      baseURL: process.env.API_BASE_URL || 'http://localhost:8080/api',
       apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8080',
     }
   },
@@ -73,7 +73,6 @@ export default defineNuxtConfig({
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
-        cors: true
       }
     }
   },
