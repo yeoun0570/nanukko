78f45dbda7308210d5355c1d3a5a6b6826a76224
여운 <yeoun0570@gmail.com>
2024-11-27 23:38:53 +0900

    :recycle: 잘못된 예외 주석처리

--- src/main/java/nanukko/nanukko_back/exception/CategoryExceptions.java
+++ src/main/java/nanukko/nanukko_back/exception/CategoryExceptions.java
@@ -1,17 +1,17 @@
 package nanukko.nanukko_back.exception;
 
 public enum CategoryExceptions {
-    NOT_FOUND("NOT_FOUND", 404);
-
-    private final Exception exception;
-
-    CategoryExceptions(String msg, int code) {
-        exception = new Exception(msg, code);
-    }
-
-    public Exception get() {
-        System.out.println("-------에러-------");
-        System.out.println(exception.getCode() + " / " + exception.getMsg());
-        return exception;
-    }
+//    NOT_FOUND("NOT_FOUND", 404);
+//
+//    private final Exception exception;
+//
+//    CategoryExceptions(String msg, int code) {
+//        exception = new Exception(msg, code);
+//    }
+//
+//    public Exception get() {
+//        System.out.println("-------에러-------");
+//        System.out.println(exception.getCode() + " / " + exception.getMsg());
+//        return exception;
+//    }
 }
