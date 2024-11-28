6fa78be5f242c0eb37a25d8daef850d7d731c8cd
여운 <yeoun0570@gmail.com>
2024-11-28 19:46:27 +0900

    :recycle: useApi 사용해서 인가된 사용자가 가도록 수정

--- src/nanukko_front/pages/my-store/sale-products/index.vue
+++ src/nanukko_front/pages/my-store/sale-products/index.vue
@@ -2,9 +2,12 @@
 import ProductsGrid from "~/components/my-store/sale-products/ProductsGrid.vue";
 import StatusFilter from "~/components/my-store/sale-products/StatusFilter.vue";
 import Pageination from "~/components/Pagination.vue";
+import { useApi } from "@/composables/useApi";
+
+const api = useApi();
 
 definePageMeta({
-  layout: 'mystore'
+  layout: "mystore",
 });
 
 const userProducts = ref([]);
@@ -16,17 +19,24 @@ const pageSize = ref(5);
 //판매 상품 출력
 const loadUserProducts = async (page = 0) => {
   try {
+    // API 요청 파라미터 로깅
+    console.log("Request params:", {
+      status: currentStatus.value,
+      page: page,
+      size: pageSize.value,
+    });
+
     //params 객체를 사용하면 Axios가 파라미터들을 자동으로 인코딩해줌
-    const response = await get(
-      `/my-store/sale-products`,
-      {
-        params: {
-          status: currentStatus.value,
-          page: page,
-          size: pageSize.value,
-        },
-      }
-    );
+    const response = await api.get(`/my-store/sale-products`, {
+      params: {
+        status: currentStatus.value,
+        page: page,
+        size: pageSize.value,
+      },
+    });
+    // 서버 응답 로깅
+    console.log("Server response:", response);
+    
     userProducts.value = response.content; //백의 PageResponseDTO의 실제 데이터를 가리키는 content
     totalPages.value = response.totalPages; //백의 PageResponseDTO의 전체 페이지수 = totalPages
     currentPage.value = response.currentPage; //백의 PageResponseDTO의 현재 페이지 = currentPage
