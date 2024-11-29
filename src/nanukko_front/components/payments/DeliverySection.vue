bc4d7950ef4ea31be3fc1eadbc439cb24036cc97
여운 <yeoun0570@gmail.com>
2024-11-29 14:30:22 +0900

    :recycle: 결제창에서 사용자의 배송지 변경 가능하도록 수정

--- src/nanukko_front/components/payments/DeliverySection.vue
+++ src/nanukko_front/components/payments/DeliverySection.vue
@@ -1,4 +1,8 @@
 <script setup>
+import { useApi } from "@/composables/useApi";
+
+const api = useApi();
+
 const props = defineProps({
   deliveryData: {
     type: Object,
@@ -6,7 +10,12 @@ const props = defineProps({
   },
 });
 
+const userAddrInfo = ref({});
+
 const isEditing = ref(false); //수정 모드
+
+// 로컬 상태로 관리
+const localAddrDetail = ref(props.deliveryData.addrDetail);
 
 // 원본 데이터 저장용 ref 추가
 const originalUserInfo = ref(null);
@@ -22,7 +31,7 @@ const toggleEdit = () => {
 
 // 취소 버튼 클릭 시 원본 데이터로 복구
 const cancelEdit = () => {
-  userInfo.value = JSON.parse(JSON.stringify(originalUserInfo.value));
+  props.deliveryData = JSON.parse(JSON.stringify(originalUserInfo.value));
   isEditing.value = false;
 };
 
@@ -34,29 +43,45 @@ const openAddressSearch = () => {
       const selectedAddress = data.roadAddress || data.jibunAddress;
 
       // 주소 정보 전달
-      emit("update:addrMain", selectedAddress); // 메인 주소 업데이트
-      emit("update:addrZipcode", data.zonecode); // 우편번호 업데이트
+      props.deliveryData.addrMain = selectedAddress;
+      props.deliveryData.addrZipcode = data.zonecode;
     },
   }).open();
 };
 
 const handleAddrDetailChange = (event) => {
   localAddrDetail.value = event.target.value;
-  emit("update:addrDetail", event.target.value);
+  props.deliveryData.addrDetail = event.target.value
+};
+
+const updateUserAddr = async () => {
+  const updateData = {
+    addrMain: props.deliveryData.addrMain,
+    addrDetail: props.deliveryData.addrDetail,
+    addrZipcode: props.deliveryData.addrZipcode,
+  };
+
+  try {
+    const response = await api.post("/payments/modify-address", updateData);
+    isEditing.value = false;
+    alert("주소가 변경되었습니다.")
+  } catch (error) {
+    console.error("사용자 배송지 정보 수정 실패: ", error);
+  }
 };
 </script>
 
 <template>
   <div class="delivery-container">
-    <div class="header">
-      <h2>배송지</h2>
-      <button class="edit-button" @click="toggleEdit">변경</button>
-    </div>
+    <h2>배송지</h2>
     <div class="delivery-section">
       <div class="delivery-info">
         <div class="name-tag">
           <span class="name">{{ deliveryData.nickname }}</span>
           <span class="badge">최근</span>
+          <button class="edit-button" @click="toggleEdit" v-if="!isEditing">
+            변경
+          </button>
         </div>
         <span class="address">
           ({{ deliveryData.addrZipcode }}) {{ deliveryData.addrMain }}
@@ -68,10 +93,12 @@ const handleAddrDetailChange = (event) => {
         >
           주소 검색
         </button>
-        <span class="address" v-if="!isEditing">{{ deliveryData.addrDetail }}</span>
+        <span class="address" v-if="!isEditing">{{
+          deliveryData.addrDetail
+        }}</span>
         <input
           v-if="isEditing"
-          :placeholder="props.deliveryData.addrDetail"
+          v-model="localAddrDetail"
           @input="handleAddrDetailChange"
           placeholder="상세 주소를 입력하세요."
           class="detail-input"
@@ -79,5 +106,14 @@ const handleAddrDetailChange = (event) => {
         <p class="phone">{{ deliveryData.mobile }}</p>
       </div>
     </div>
+  </div>
+  <div class="delivery-btn-container">
+    <button class="save-button" v-if="isEditing" @click="updateUserAddr">
+      저장
+    </button>
+
+    <button class="cancel-button" @click="cancelEdit" v-if="isEditing">
+      취소
+    </button>
   </div>
 </template>
