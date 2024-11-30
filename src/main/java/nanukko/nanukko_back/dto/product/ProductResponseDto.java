e090a2be52fdd9f1b8478fcdcb07ca92d8445c7a
KeK <127266806+disneydreamworker@users.noreply.github.com>
2024-11-30 23:29:26 +0900

    :sparkles: ProductMapper 및 ProductResponseDto 생성

---
+++ src/main/java/nanukko/nanukko_back/dto/product/ProductResponseDto.java
@@ -0,0 +1,64 @@
+package nanukko.nanukko_back.dto.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductResponseDto {
+    // 기본 정보
+    private Long id;
+    private String productName;
+    private int price;
+    private String content;
+    private String condition; // Condition의 String 값
+
+    // 유저 정보
+    private String userId;
+    private Double reviewRate;
+    private String profile;
+
+    // 이미지 정보
+    private String image1;
+    private String image2;
+    private String image3;
+    private String image4;
+    private String image5;
+
+    // 카테고리 정보
+    private Long majorId; //
+    private Long middleId; // 중분류 카테고리 id
+
+    // 배송 관련 정보
+    private Boolean isPerson;
+    private Boolean isShipping;
+    private Boolean freeShipping;
+    private Integer shippingFee;
+
+    // 거래 관련 정보
+    private Boolean isCompanion;
+    private Boolean isDeputy;
+    private Boolean gender;
+    private String ageGroup;
+
+    // 거래 장소 정보
+    private String zipCode;
+    private String address;
+    private String detailAddress;
+    private Double latitude;
+    private Double longitude;
+
+    // 찜 여부
+    private Boolean isWished;
+
+    //좋아요 수
+    private Integer favorite_count;
+    private Integer view_count;
+    private Integer talk_count;
+}
