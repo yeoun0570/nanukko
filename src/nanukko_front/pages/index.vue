9883b9e5e4d4bac5de5342c1b3fef330b667610f
KeK <127266806+disneydreamworker@users.noreply.github.com>
2024-12-02 02:13:24 +0900

    :sparkles: Main 상품 리스트 추가

--- src/nanukko_front/pages/index.vue
+++ src/nanukko_front/pages/index.vue
@@ -2,7 +2,8 @@
 import Header from "~/components/layout/header.vue";
 import Footer from "~/components/layout/footer.vue";
 import navigator from "~/components/layout/navigator.vue";
-import banner from "~/components/mainpage/banner.vue"
+import banner from "~/components/mainpage/banner.vue";
+import Main from "~/components/products/main/main.vue";
 
 
 import { useAuth } from '~/composables/auth/useAuth';
@@ -14,23 +15,23 @@ const { isAuthenticated, userId, nickname } = useAuth();
 </script>
 
 <template>
-
   <div>
     <h1 v-if="isAuthenticated">{{ userId }}님 환영합니다.</h1>
     <h1 v-else>로그인이 필요합니다.</h1>
   </div>
 
-
   <div class="home-container">
     <h1>채팅버튼</h1>
-    
+
     <!-- 테스트 채팅 버튼 추가 -->
     <TestChatButton />
   </div>
 
 
 
-<banner />
+  <banner />
+
+  <Main />
 
 </template>
 
@@ -45,5 +46,4 @@ h1 {
   text-align: center;
   margin-bottom: 2rem;
 }
-
 </style>
